82661b822e189c625f83e415aed8d1b5b4e4674b
Giacomo Cangi <giacomocangi.96@gmail.com>
2025-01-03 16:12:36 +0100

    test_AA.py creation

---
+++ test_AA.py
@@ -0,0 +1,9 @@
+import numpy as np
+import scipy as sc
+from maker import *
+from solver import *
+from pmd_functions import *
+import matplotlib.pyplot as plt
+
+
+#* Multi-Body model creation - Bodies, Joints, Forces
