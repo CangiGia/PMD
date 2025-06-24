5019bd6853984d213e3313be37eafde748237f01
GiacomoCangi <giacomocangi.96@gmail.com>
2025-06-24 10:44:10 +0200

    .gitignore file updated

--- examples/_test_AA.py
+++ examples/_test_AA.py
@@ -5,131 +5,61 @@ from PMD.src.solver import *
 from PMD.src.functions import *
 import matplotlib.pyplot as plt
 
-
-#* Multi-Body model creation - Bodies, Joints, Forces
 #%% bodies
-B1 = Body() # lower suspension arm
-B1.m = 2
-B1.J = 0.5
-B1.r = np.array([0.4398, 0.2512])
-B1.p = 6.2465
-
-B2 = Body() # wheel assembly
-B2.m = 30
-B2.J = 2.5 
-B2.r = np.array([0.6817, 0.3498])
-B2.p = 0.0783
-
-B3 = Body() # upper suspension arm
-B3.m = 1
-B3.J = 0.5 
-B3.r = np.array([0.4463, 0.4308])
-B3.p = 6.5222
+B1 = Body(m=2, J=0.5, r=[0.4398, 0.2512], p=6.2465)  # lower suspension arm
+B2 = Body(m=30, J=2.5, r=[0.6817, 0.3498], p=0.0783)  # wheel assembly
+B3 = Body(m=1, J=0.5, r=[0.4463, 0.4308], p=6.5222)  # upper suspension arm
 
 #%% points
-q1 = Point() # Q point - lower suspension arm side, body 1
-q1.Bindex = 1
-q1.sPlocal = np.array([-0.24, 0.0])
-
-a1 = Point() # A point - lower suspension arm side, body 1
-a1.Bindex = 1
-a1.sPlocal = np.array([0.18, 0.0])
-
-a2 = Point() # A point - wheel assembly side, body 2
-a2.Bindex = 2
-a2.sPlocal = np.array([-0.07, -0.10])
-
-b2 = Point() # B point - wheel assembly side, body 2
-b2.Bindex = 2
-b2.sPlocal = np.array([-0.10, 0.12])
-
-b3 = Point() # B point - upper suspension arm side, body 3
-b3.Bindex = 3
-b3.sPlocal = np.array([0.13, 0.0])
-
-o3 = Point() # O point - upper suspension arm side, body 3
-o3.Bindex = 3
-o3.sPlocal = np.array([-0.13, 0.0])
-
-o0 = Point() # O point - ground
-o0.Bindex = 0
-o0.sPlocal = np.array([0.32, 0.40])
-
-q0 = Point() # Q point - ground
-q0.Bindex = 0
-q0.sPlocal = np.array([0.20, 0.26])
-
-e1 = Point() # E point - Lower suspension arm side, body 1
-e1.Bindex = 1 
-e1.sPlocal = np.array([0.0, 0.0])
-
-f0 = Point() # F point - Ground
-f0.Bindex = 0
-f0.sPlocal = np.array([0.38, 0.43])
+q1 = Point(Bindex=1, sPlocal=np.array([-0.24, 0.0]))   # Q point - lower suspension arm side
+a1 = Point(Bindex=1, sPlocal=np.array([0.18, 0.0]))    # A point - lower suspension arm side
+a2 = Point(Bindex=2, sPlocal=np.array([-0.07, -0.10])) # A point - wheel assembly side
+b2 = Point(Bindex=2, sPlocal=np.array([-0.10, 0.12]))  # B point - wheel assembly side
+b3 = Point(Bindex=3, sPlocal=np.array([0.13, 0.0]))    # B point - upper suspension arm side
+o3 = Point(Bindex=3, sPlocal=np.array([-0.13, 0.0]))   # O point - upper suspension arm side
+o0 = Point(Bindex=0, sPlocal=np.array([0.32, 0.40]))   # O point - ground
+q0 = Point(Bindex=0, sPlocal=np.array([0.20, 0.26]))   # Q point - ground
+e1 = Point(Bindex=1, sPlocal=np.array([0.0, 0.0]))     # E point - Lower suspension arm side
+f0 = Point(Bindex=0, sPlocal=np.array([0.38, 0.43]))   # F point - Ground
 
 #%% joints
-j1 = Joint() #! Revolute joint in Q 
-j1.type = 'rev'
-j1.iPindex = 0
-j1.jPindex = 7
+j1 = Joint(type='rev', iPindex=0, jPindex=7)  # Revolute joint in Q
+j2 = Joint(type='rev', iPindex=1, jPindex=2)  # Revolute joint in A
+j3 = Joint(type='rev', iPindex=3, jPindex=4)  # Revolute joint in B
+j4 = Joint(type='rev', iPindex=5, jPindex=6)  # Revolute joint in O
 
-j2 = Joint() #! Revolute joint in A 
-j2.type = 'rev'
-j2.iPindex = 1
-j2.jPindex = 2
+#%% forces
+s1 = Force(type='ptp', iPindex=8, jPindex=9, k=90000, L0=0.23, dc=1100)  # spring-damper force
 
-j3 = Joint() #! Revolute joint in B 
-j3.type = 'rev'
-j3.iPindex = 3
-j3.jPindex = 4
-
-j4 = Joint() #! Revolute joint in O 
-j4.type = 'rev'
-j4.iPindex = 5
-j4.jPindex = 6
-
-#%% forces - custom forces
-s1 = Force()
-s1.type = 'ptp'
-s1.iPindex = 8
-s1.jPindex = 9
-s1.k = 90000
-s1.L0 = 0.23
-s1.dc = 1100
-
-# custom mathematical model
+# custom wheel contact force
 def my_force(B2, s2):
-    """
-    Custom force used to define the wheel contact condition.
-    """
-
-    # unilateral spring-damper in the y-direction
+    """Custom force used to define the wheel contact condition."""
     dely = B2.r[1] - s2.L0
     if dely < 0:
         fy = (s2.k * dely + s2.dc * B2.dr[1]).item()
         fsd = np.array([0, -fy])
         B2._f = B2._f + fsd.reshape(2,1)
 
-s2 = Force() # wheel contact force
-s2.type = 'user'
-s2.k = 50000
-s2.L0 = 0.35
-s2.dc = 1000
+s2 = Force(type='user', k=50000, L0=0.35, dc=1000)  # wheel contact force
 s2.callback = my_force
 
-s3 = Force()
-s3.type = 'weight'
-s3.wgt = np.array([0, -1])
+s3 = Force(type='weight')  # gravity force
 
-#%% solution
-my_dynamic_model = PlanarMultibodyModel(verbose=True)
-time, solution = my_dynamic_model.solve(method='RK45')
+#%% solution and plotting
+quarter_car = PlanarMultibodyModel()
+time, solution = quarter_car.solve(method='Radau')
+reactions = quarter_car.get_reactions()
 
-plt.figure()
-plt.plot(time, solution[:,4])
+plt.figure(figsize=(15, 5))
+plt.plot(time, solution[:,4], label='Wheel vertical position', color='y', linestyle='-', linewidth=2)
+plt.ylabel('Displacement (m)', fontsize=12)
+plt.legend()
+plt.title('Wheel Assembly Response', fontsize=14)
 plt.show()
 
-# np.savetxt('uT_python.txt', solution)
-# np.savetxt('T_python.txt', time)
-
-ecchime = 1
+plt.figure(figsize=(15, 5))
+plt.plot(time, reactions[:, 0], label='Reaction force at Q', color='b', linestyle='-', linewidth=2)
+plt.ylabel('Force (N)', fontsize=12)
+plt.legend()
+plt.title('Reaction force at Q', fontsize=14)
+plt.show()
--- file_puttana.py
+++ file_puttana.py
@@ -2,17 +2,17 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-current_directory = os.path.dirname(os.path.abspath(__file__))
+# current_directory = os.path.dirname(os.path.abspath(__file__))
 
-time_ADAMS = np.loadtxt(os.path.join(current_directory, "time_ADAMS.txt"), skiprows=6)
-x1_ADAMS = np.loadtxt(os.path.join(current_directory, "x1_ADAMS.txt"), skiprows=6)
-x2_ADAMS = np.loadtxt(os.path.join(current_directory, "x2_ADAMS.txt"), skiprows=6)
+# time_ADAMS = np.loadtxt(os.path.join(current_directory, "time_ADAMS.txt"), skiprows=6)
+# x1_ADAMS = np.loadtxt(os.path.join(current_directory, "x1_ADAMS.txt"), skiprows=6)
+# x2_ADAMS = np.loadtxt(os.path.join(current_directory, "x2_ADAMS.txt"), skiprows=6)
 
-uT_python = np.loadtxt(os.path.join(current_directory, "uT_python.txt"))
-T_python = np.loadtxt(os.path.join(current_directory, "T_python.txt"))
+# uT_python = np.loadtxt(os.path.join(current_directory, "uT_python.txt"))
+# T_python = np.loadtxt(os.path.join(current_directory, "T_python.txt"))
 
-uT_matlab = np.loadtxt(os.path.join(current_directory, "uT_matlab.txt"))
-T_matlab = np.loadtxt(os.path.join(current_directory, "T_matlab.txt"))
+# uT_matlab = np.loadtxt(os.path.join(current_directory, "uT_matlab.txt"))
+# T_matlab = np.loadtxt(os.path.join(current_directory, "T_matlab.txt"))
 
 # -----------------------------
 # ho 12 colonne nei file sopra
@@ -21,12 +21,24 @@ T_matlab = np.loadtxt(os.path.join(current_directory, "T_matlab.txt"))
 # X1 Y1 Phi1 X2 Y2 Phi2 || dX1 dY1 dPhi1 dX2 dY2 dPhi2
 # -----------------------------
 
+# fig, ax = plt.subplots(figsize=(15, 4.5))
+# ax.plot(time_ADAMS, x1_ADAMS, label="adams")
+# ax.plot(T_matlab, uT_matlab[:, 0], label="matlab")
+# ax.plot(T_python, uT_python[:, 0], label="python")
+# ax.set_xlabel("Time [s]")
+# ax.set_ylabel("Dispacement")
+# ax.set_title("Comparison between Adams, Python and Matlab")
+# ax.legend()
+# plt.show()
+
+lam = np.loadtxt("AA_lambda_matlab.txt")
+time = np.loadtxt("AA_time_matlab.txt").T
+
 fig, ax = plt.subplots(figsize=(15, 4.5))
-ax.plot(time_ADAMS, x1_ADAMS, label="adams")
-ax.plot(T_matlab, uT_matlab[:, 0], label="matlab")
-ax.plot(T_python, uT_python[:, 0], label="python")
+ax.plot(time, lam[:,0], label="Fx")
+ax.plot(time, lam[:,1], label="Fy")
 ax.set_xlabel("Time [s]")
-ax.set_ylabel("Dispacement")
-ax.set_title("Comparison between Adams, Python and Matlab")
+ax.set_ylabel("Force [N]")
+ax.set_title("Reactioin force at Q revolute joint")
 ax.legend()
 plt.show()
