a459676f49f1dba096dad9531412576232490ecf
Giacomo Cangi <giacomocangi.96@gmail.com>
2026-04-23 00:55:55 +0200

    feat(gui): add icons to File/View/Simulations nav buttons

--- gui/panels/simulation_panel.py
+++ gui/panels/simulation_panel.py
@@ -54,9 +54,9 @@ class SimulationPanel(QWidget):
         top_layout.setContentsMargins(0, 0, 0, 0)
         top_layout.setSpacing(0)
 
-        self._btn_file        = self._make_nav_btn("File")
-        self._btn_view        = self._make_nav_btn("View")
-        self._btn_simulations = self._make_nav_btn("Simulations")
+        self._btn_file        = self._make_nav_btn("File",        "mdi6.home-outline")
+        self._btn_view        = self._make_nav_btn("View",        "mdi6.eye-outline")
+        self._btn_simulations = self._make_nav_btn("Simulations", "mdi6.play-circle-outline")
         for btn in (self._btn_file, self._btn_view, self._btn_simulations):
             top_layout.addWidget(btn)
         top_layout.addStretch(1)
